a3871432d5c772be6998cb5e54e62e0d5e068e54
Wendy Ta <wendy.ta524@gmail.com>
2026-04-18 16:40:22 -0700

    Add DataCollector to homepage

--- home_page.py
+++ home_page.py
@@ -1,10 +1,20 @@
 from flask import *
+from data import DataCollector
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
+
 def flight_data():
     return render_template("home_page.html")
 
-if __name__== "__main__":
+@app.route("/collect", methods=["POST"])
+def generate_data():
+    collector = DataCollector()
+    collector.get_data()
+    collector.create_csv()
+
+    return redirect(url_for("flight_data"))
+
+if __name__ == "__main__":
     app.run(debug=True)
