a72c6903bed12e128490cd76c34565f692b205e9
Wendy Ta <wendy.ta524@gmail.com>
2026-05-04 23:56:01 -0700

    update data and plots

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ import time, csv
 class DataCollector:
     def __init__(self):
         try: # use credentials if they exist
-            tm = TokenManager.from_json_file("credentials (3).json")
+            tm = TokenManager.from_json_file("credentials (2).json")
             self.api = OpenSkyApi(token_manager=tm)
         except:
             self.api = OpenSkyApi()
@@ -13,10 +13,9 @@ class DataCollector:
         self.hourly_flights = {} # store number of flights for each hour
 
         self.cur_time = int(time.time()) # get current time RIGHT NOW in seconds
-        self.cur_time = self.cur_time - (self.cur_time % (3600*24))  # round down to nearest day at midnight
         self.start_time = self.cur_time - (3600 * 24 * 7) # go back 7 days from now in seconds
         self.step = 1 * 3600 # size of each chunk of data (1 hour in seconds)
-    
+
     def get_data(self):
         print("fetching data from api...")
         start = datetime.now()
@@ -26,13 +25,15 @@ class DataCollector:
         while t < self.cur_time: # keep looping until we reach "now"
             next_t = min(t + self.step, self.cur_time) # move forward step hrs but do not go past the current time
             data = self.api.get_departures_by_airport("KSFO", t, next_t)
-            # time.sleep(0.02) # go to sleep in between api calls to avoid the rate call per-minute limit
+            # time.sleep(5) # go to sleep in between api calls to avoid the rate call per-minute limit
 
             time_label = datetime.fromtimestamp(t, tz=timezone.utc).strftime("%m-%d-%Y %H:%M")
             if data: # check if data was actually returned
                 self.hourly_flights[time_label] = len(data)
             else: # otherwise number of flights = 0
                 self.hourly_flights[time_label] = 0
+                print("No data returned for", time_label)
+                # return
 
             t = next_t # move forward 
 
@@ -52,4 +53,4 @@ class DataCollector:
                 writer.writerow([hour, flights])
                 print(hour, flights)
 
-        print("csv file creation complete!")
+        print("csv file created!")
